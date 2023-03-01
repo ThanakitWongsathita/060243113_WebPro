3aa15e7dc661d34de666377e31881ad5d5e7de76
ThanakitWongsathita <thanakitwongsathita2@gmail.com>
2023-03-01 15:29:36 +0700

    update

--- customer.sql
+++ customer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2023 at 05:40 AM
+-- Generation Time: Mar 01, 2023 at 09:28 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -40,9 +40,9 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`customerId`, `customerName`, `customerAddress`, `customerEmail`, `customerTelephone`) VALUES
-(2, 'กิอีปำกํา', 'ddsddssdsd', 'qwetewrr@gmail.com', '1234567890'),
-(3, 'กิอีปำกําqw', 'qweqwrqwr', 'qwetewrr@gmail.com', '1234567890'),
-(4, 'กิอีปำกําqw', 'qweqwrqwr', 'qwetewrr@gmail.com', '1234567890');
+(2, 'Thanakit', 'ddsddssdsd', 'thanakit@gmail.com', '0123456789'),
+(3, 'qewqweqeqw', 'ddsddssdsd', 'qwetewrr@gmail.com', '1234567890'),
+(5, 'กเดำกห', 'ddsddssdsd', 'หดกหดด@gmail.com', '1234567890');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +62,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
